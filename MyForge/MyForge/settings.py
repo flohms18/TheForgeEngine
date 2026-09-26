820247f2ba2443636a9d576fe5c6172f2463eb86
flohms18 <trouilletflorent@outlook.com>
2026-09-26 19:12:39 +0200

    Fix bug

--- MyForge/MyForge/settings.py
+++ MyForge/MyForge/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_browser_reload.middleware.BrowserReloadMiddleware'
 ]
 
 ROOT_URLCONF = 'MyForge.urls'
--- MyForge/MyForge/urls.py
+++ MyForge/MyForge/urls.py
@@ -22,10 +22,3 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-
-
-if settings.DEBUG:
-    # Include django_browser_reload URLs only in DEBUG mode
-    urlpatterns += [
-        path("__reload__/", include("django_browser_reload.urls")),
-    ]
